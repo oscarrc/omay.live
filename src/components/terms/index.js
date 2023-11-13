fda0a51ed854ad1a7886f9bb444cdda2022a081b
Oscar <oscarrc.web@gmail.com>
2023-11-13 18:32:17 +0100

    [MOD] modal font size

--- src/components/terms/index.js
+++ src/components/terms/index.js
@@ -26,13 +26,13 @@ const Terms = ({ show }) => {
                     <div className="form-control">
                         <label className="label cursor-pointer gap-4 items-start justify-start">
                             <input type="checkbox" checked={tac} onChange={() => setTac(!tac)} className="checkbox" />
-                            <span className="label-text">By checking the box you acknowledge that you have reviewed an agree to be bound by {BRAND}&apos; <Link target="_blank" rel="noopener noreferrer" className="btn-link" to={"policies/terms-and-conditions"}>Terms and conditions</Link>, <Link target="_blank" rel="noopener noreferrer" className="btn-link" to={"policies/privacy-policy"}>Privacy policy</Link> and <Link target="_blank" rel="noopener noreferrer" className="btn-link" to={"policies/community-guidelines"}>Community guidelines</Link>.</span> 
+                            <span className="label-text text-lg">By checking the box you acknowledge that you have reviewed an agree to be bound by {BRAND}&apos; <Link target="_blank" rel="noopener noreferrer" className="btn-link" to={"policies/terms-and-conditions"}>Terms and conditions</Link>, <Link target="_blank" rel="noopener noreferrer" className="btn-link" to={"policies/privacy-policy"}>Privacy policy</Link> and <Link target="_blank" rel="noopener noreferrer" className="btn-link" to={"policies/community-guidelines"}>Community guidelines</Link>.</span> 
                         </label>
                     </div>
                     <div className="form-control">
                         <label className="label cursor-pointer gap-4 items-start justify-start">
                             <input checked={age} onChange={() => setAge(!age)} type="checkbox" className="checkbox" />
-                            <span className="label-text">You may not use this service if you are under the age of 18 or the age of legal majority in your jurisdiction. See our <Link target="_blank" rel="noopener noreferrer" className="btn-link" to={"policies/terms-and-conditions"}>Terms and conditions</Link> for more info. <strong>By checking the box you acknowledge and represent that you are 18 years (or the legal majority in your jurisdiction) or older.</strong></span> 
+                            <span className="label-text text-lg">You may not use this service if you are under the age of 18 or the age of legal majority in your jurisdiction. See our <Link target="_blank" rel="noopener noreferrer" className="btn-link" to={"policies/terms-and-conditions"}>Terms and conditions</Link> for more info. <strong>By checking the box you acknowledge and represent that you are 18 years (or the legal majority in your jurisdiction) or older.</strong></span> 
                         </label>
                     </div>
                     <div className="form-control items-end mt-4">                        
