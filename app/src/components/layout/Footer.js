4edb74d30b2ed1a83aeb0f0948dfd507d8ffd06f
Oscar <oscarrc.web@gmail.com>
2023-11-15 19:31:04 +0100

    [MOD] Translations

--- app/src/components/layout/Footer.js
+++ app/src/components/layout/Footer.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <footer className="footer flex flex-col md:flex-row justify-between items-center gap-0 md:justify-between bg-base-200 px-8 py-2">
-            <p className="text-center md:text-left">Copyright © { (new Date() ).getFullYear() } - All right reserved by { BRAND }</p>
+            <p className="text-center md:text-left">Copyright © { (new Date() ).getFullYear() } { BRAND } - All right reserved.</p>
             <div className="flex flex-col items-center sm:flex-row gap-0 sm:gap-4">
                 <Link to={"policies/terms-and-conditions"}>Terms and conditions</Link>
                 <Link to={"policies/privacy-policy"}>Privacy policy</Link>
--- app/src/routes/landing/index.js
+++ app/src/routes/landing/index.js
@@ -1,5 +1,6 @@
 import { Alert, InterestInput } from "../../components/partials";
 import { BRAND, TRANSLITERATION } from "../../constants/brand";
+import { Trans, useTranslation } from 'react-i18next';
 
 import { AiFillWarning } from "react-icons/ai";
 import { FaBan } from "react-icons/fa";
@@ -10,6 +11,7 @@ import { useEffect } from "react";
 
 const Landing = () => {     
     const { state: { tac, mode }, dispatch, interests, setInterests } = useChat();
+    const { t } = useTranslation()
     
     useEffect(()=> {
         dispatch({type: "RESET"})
@@ -20,10 +22,26 @@ const Landing = () => {
             <section className="flex gap-4 justify-center items-center flex-col flex-1 w-full bg-base-100 rounded shadow-inner p-8">
                 <div className="flex flex-col gap-8">
                     <div className="prose md:prose-lg min-w-fit">
-                        <h4 className="text-center">You don't need an app to use { BRAND } on your phone or tablet! The web site works great on mobile.</h4>
-                        <p>{BRAND} ({TRANSLITERATION}) is a great way to meet new friends. When you use { BRAND }, we pick someone else at random and let you talk one-to-one. To help you stay safe, chats are anonymous unless you tell someone who you are (not recommended!), and you can stop a chat any time. See our  <Link className="btn-link" to={"policies/terms-and-conditions"}>Terms and conditions</Link>, and <Link className="btn-link" to={"policies/community-guidelines"}>Community guidelines</Link> for info about de do's and don'ts in using { BRAND }. { BRAND } video chat is AI moderated but no moderation is perfects. Users are solely responsible for their behaviour while using { BRAND }</p>
-                        <p><strong>You must be 18 (or the legal majority in your jurisdiction) or older to use {BRAND}</strong>. See {BRAND} <Link className="btn-link" to={"policies/terms-and-conditions"}>Terms and conditions</Link> for more info. Parental control protections that may assists parents are commercially available and you can find more info at <a className="btn-link" href="https://www.connectsafely.org/controls" target="_blank" rel="noopener noreferrer" >https://www.connectsafely.org/controls</a> as well are other sites.</p>
-                        <p>Please leave { BRAND } and visit an adult site instead if that's what you are looking for and you are 18 (or the legal majority in your jurisdiction) or older.</p>
+                        <h4 className="text-center">
+                            <Trans i18nKey="landing:noapp">
+                                You don't need an app to use { BRAND } on your phone or tablet! The web site works great on mobile.
+                            </Trans>
+                        </h4>
+                        <p>
+                            <Trans i18nKey="landing:intro">
+                                {BRAND} ({TRANSLITERATION}) is a great way to meet new friends. When you use { BRAND }, we pick someone else at random and let you talk one-to-one. To help you stay safe, chats are anonymous unless you tell someone who you are (not recommended!), and you can stop a chat any time. See our  <Link className="btn-link" to={"policies/terms-and-conditions"}>Terms and conditions</Link>, and <Link className="btn-link" to={"policies/community-guidelines"}>Community guidelines</Link> for info about de do's and don'ts in using { BRAND }. { BRAND } video chat is AI moderated but no moderation is perfects. Users are solely responsible for their behaviour while using { BRAND }
+                            </Trans>
+                        </p>
+                        <p>
+                            <Trans i18nKey="landing:disclaimer">
+                                <strong>You must be 18 (or the legal majority in your jurisdiction) or older to use {BRAND}</strong>. See {BRAND} <Link className="btn-link" to={"policies/terms-and-conditions"}>Terms and conditions</Link> for more info. Parental control protections that may assists parents are commercially available and you can find more info at <a className="btn-link" href="https://www.connectsafely.org/controls" target="_blank" rel="noopener noreferrer" >https://www.connectsafely.org/controls</a> as well are other sites.
+                            </Trans>
+                        </p>
+                        <p>
+                            <Trans i18nKey="landing:pleaseleave">
+                                Please leave { BRAND } and visit an adult site instead if that's what you are looking for and you are 18 (or the legal majority in your jurisdiction) or older.
+                            </Trans>
+                        </p>
                     </div>
                     <Alert 
                         title="Video is monitored"
@@ -39,7 +57,7 @@ const Landing = () => {
                     /> */}
                     <div className="flex flex-col md:flex-row gap-8 justify-between">
                         <div className="flex flex-col gap-3 items-center">
-                            <h4>What do you want to talk about?</h4>
+                            <h4>{ t("common.talkabout") }</h4>
                             <InterestInput values={interests} onChange={setInterests} />
                         </div>
                         <div className="flex flex-col gap-2 items-center">
