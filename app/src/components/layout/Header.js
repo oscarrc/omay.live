51ebf09d7457e5e04d27ce31fb50bf2796fb530e
Oscar <oscarrc.web@gmail.com>
2023-11-15 12:08:21 +0100

    [ADD] Logo

--- app/src/components/layout/Header.js
+++ app/src/components/layout/Header.js
@@ -6,7 +6,10 @@ const Header = () => {
         <header className="sticky top-0 bg-base-100 z-40">
             <nav className="navbar shadow-lg">
                 <div className="flex gap-8 flex-1">
-                    <Link to="/" className="btn btn-ghost text-xl uppercase">{ BRAND }</Link>
+                    <Link to="/" className="btn btn-ghost text-warning text-xl uppercase py-2">
+                        <img src="./logo.svg" alt={BRAND} className="h-full"/>
+                        { BRAND }
+                    </Link>
                     <span className="hidden md:inline -rotate-6 text-primary lg:text-xl font-bold">Connect with strangers!</span>
                 </div>
                 <div className="flex gap-8 flex-none px-4">
