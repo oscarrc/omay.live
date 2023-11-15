5a52c3e50f6fb5ef262d3a528fa20e6dcc2e13d8
Oscar <oscarrc.web@gmail.com>
2023-11-15 12:08:26 +0100

    [ADD] Status strings

--- app/src/constants/strings.js
+++ app/src/constants/strings.js
@@ -1,3 +1,16 @@
-const BRAND = "Omegle";
+const BRAND = "Lamdazle";
+const TRANSLITERATION = "læm·də·zəl";
 
-export { BRAND }
+const SERCHING = {
+    connecting: "Connecting to the server",
+    error: "Unable to connect to the server",
+    banned: "Banned",
+    searching: "Looking for a random stranger you can chat with...",
+    connected: "You're chatting with a random stranger",
+    language: "You both speak the same language. Select any on the language menu to disbale.",
+    interest: "You both like",
+    simulated: "Stranger my be using a simulated webcam"
+}
+
+
+export { BRAND, SERCHING, TRANSLITERATION }
