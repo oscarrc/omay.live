ce11e5dae025ffa538ced2b19b584d90bf1559bd
Oscar <oscarrc.web@gmail.com>
2023-12-29 13:45:38 +0100

    [MOD] async / await

--- server/index.js
+++ server/index.js
@@ -37,68 +37,69 @@ const io = new socket(server, {
 
 if(io) console.log(`${chalk.green.bold("[SOCKET]")} Ready on ${BASE_URL}:${PORT}`)
 
-io.on('connection', (socket) => { 
+io.on('connection', async (socket) => {   
+  let banned = await BanService.isBanned(socket.handshake.address);
+     
+  if(banned) {      
+    console.log(`${chalk.red.bold("> [banned]")} ${chalk.bgRed.black.bold(' nsfw ')} ${chalk.red(`${socket.id} (${socket.handshake.address})`)}`)
+    socket.emit("banned");
+  }
+
+  await ChatService.peerConnected({
+    peer: socket.id,
+    ip: socket.handshake.address,
+    mode: socket.handshake.query.mode,
+    interests: socket.handshake.query.interests,
+    lang: socket.handshake.query.lang,
+    simulated: socket.handshake.query.simulated || false
+  })
+
   console.log(`${chalk.green.bold("> [connected]")} ${chalk.bgGreen.black.bold(` ${socket.handshake.query.mode} `)} ${chalk.green(`${socket.id} (${socket.handshake.address})`)}`)
 
-  BanService.isBanned(socket.handshake.address).then( async (banned) => {    
-    if(banned) {      
-      console.log(`${chalk.red.bold("> [banned]")} ${chalk.bgRed.black.bold(' nsfw ')} ${chalk.red(`${socket.id} (${socket.handshake.address})`)}`)
-      socket.emit("banned");
-    }
-
-    await ChatService.peerConnected({
-      peer: socket.id,
-      ip: socket.handshake.address,
-      mode: socket.handshake.query.mode,
-      interests: socket.handshake.query.interests,
-      lang: socket.handshake.query.lang,
-      simulated: socket.handshake.query.simulated || false
-    })
-  });
-
-  socket.on('report', (data) => {
+  socket.on('report', async (data) => {
     if(!data.id) return;
+
+    let peer = await ChatService.getPeer(data.id);
+    let banned = await BanService.warn(peer.ip);
+
     console.log(`${chalk.yellow.bold("> [reported]")} ${chalk.bgYellow.black.bold(' nsfw ')} ${chalk.yellow(data.id)}`)
+    
+    if(!banned) return;
 
-    ChatService.getPeer(data.id).then( (peer) => {
-      BanService.warn(peer.ip).then( banned => {
-        if(!banned) return 
+    console.log(`${chalk.red.bold("> [banned]")} ${chalk.bgRed.black.bold(' nsfw ')} ${chalk.red(`${data.id} (${peer.ip})`)}`)
+    socket.emit("banned");
 
-        console.log(`${chalk.red.bold("> [banned]")} ${chalk.bgRed.black.bold(' nsfw ')} ${chalk.red(`${data.id} (${peer.ip})`)}`)
-        socket.emit("banned");
-        ChatService.peerDisconnected(socket.id)
-      })
-    });
+    await ChatService.peerDisconnected(socket.id)
   })
  
   socket.on('candidatesent', (data) => {
     socket.to(data.remoteId).emit("receivecandidate", data)
   })
 
-  socket.on('offercreated', (data) => {
+  socket.on('offercreated', async (data) => {
     socket.to(data.remoteId).emit("receiveoffer", data);
-    ChatService.peerAvailable(data.id);
+    await ChatService.peerAvailable(data.id);
   })
 
-  socket.on('answercreated', (data) => {    
-    socket.to(data.remoteId).emit("receiveanswer", data)
+  socket.on('answercreated', async (data) => {    
+    await socket.to(data.remoteId).emit("receiveanswer", data)
     ChatService.peerUnavailable(data.id);
   })
 
-  socket.on('answerreceived', (data) => {
-    ChatService.peerUnavailable(data.id);
+  socket.on('answerreceived', async (data) => {
+    await ChatService.peerUnavailable(data.id);
   })
 
   socket.on('connectionclosed', (data) => {
     socket.to(data.remoteId).emit('peerdisconnected')
   })
 
-  socket.on('disconnect', () => {
+  socket.on('disconnect', async () => {
+    await ChatService.peerDisconnected(socket.id)
     console.log(`${chalk.blue.bold("> [disconnected]")} ${chalk.bgBlue.black.bold(" exit ")} ${chalk.blue(`${socket.id} (${socket.handshake.address})`)}`)
-    ChatService.peerDisconnected(socket.id)
   });
 
-  socket.on('peerupdated', (data) => {
-    ChatService.updatePeer(socket.id, data)
+  socket.on('peerupdated', async (data) => {
+    await ChatService.updatePeer(socket.id, data)
   })
 });
