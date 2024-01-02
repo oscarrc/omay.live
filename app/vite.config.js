548bfa67ab54b8f54f2cefe18ae733722a8600d8
Oscar <oscarrc.web@gmail.com>
2024-01-02 14:15:44 +0100

    [FIX] MDX

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,9 +1,20 @@
+import { VitePWA } from 'vite-plugin-pwa'
 import { defineConfig } from 'vite'
+import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     base: '/',
-    plugins: [react()],
+    plugins: [
+        {enforce: 'pre', ...mdx(/* jsxImportSource: …, otherOptions… */)},
+        react(),
+        VitePWA({ 
+            registerType: 'autoUpdate',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg,json}']
+            }
+        })
+    ],
     server: {    
         open: true, 
         port: 3000, 
@@ -11,8 +22,8 @@ export default defineConfig({
     resolve: {
         alias: [
             {
-            find: 'nsfwjs',
-            replacement: 'nsfwjs/dist/nsfwjs.min.js',
+                find: 'nsfwjs',
+                replacement: 'nsfwjs/dist/nsfwjs.min.js',
             },
         ],
     }
