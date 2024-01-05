32fb14e02fff9b95a7fb6bf4b4f16111e2249cd7
Oscar <oscarrc.web@gmail.com>
2024-01-05 16:39:51 +0100

    [MOD] Translation script

--- translate.sh
+++ translate.sh
@@ -1,10 +1,14 @@
 LOCALES_FOLDER=./app/public/locales
+MISSING_FILE="$LOCALES_FOLDER/missing.txt"
 
 while IFS= read -r name; do 
-    if [ -f "$LOCALES_FOLDER/$name/translation.json" ] then
+    if [ -f "$LOCALES_FOLDER/$name/translation.json" ]; then
         continue
     fi
-    jsontt "$LOCALES_FOLDER/en/translation.json" --module google --from en --to $name --name translation -cl 1 -fb yes \    
-    && mkdir -p "$LOCALES_FOLDER/$name" \
-    && mv "$LOCALES_FOLDER/en/translation.$name.json" "$LOCALES_FOLDER/$name/translation.json"
+    
+    jsontt "$LOCALES_FOLDER/en/translation.json" --module google --from en --to $name --name translation -cl 1 -fb yes &&
+    mkdir -p "$LOCALES_FOLDER/$name" &&
+    mv "$LOCALES_FOLDER/en/translation.$name.json" "$LOCALES_FOLDER/$name/translation.json" ||
+    touch $MISSING_FILE $name >> $MISSING_FILE
+
 done <"$LOCALES_FOLDER/locales.txt"
