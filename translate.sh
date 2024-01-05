5e56ccf4c126ddf63e3f0351cfa3ce1b12f6c92f
Oscar <oscarrc.web@gmail.com>
2024-01-05 16:58:23 +0100

    [MOD] Translate error check

--- translate.sh
+++ translate.sh
@@ -6,10 +6,14 @@ while IFS= read -r name; do
         continue
     fi
     
-    node_modules/@parvineyvazov/json-translator/bin/jsontt "$LOCALES_FOLDER/en/translation.json" --module google --from en --to $name --name translation -cl 1 -fb yes &&
-    mkdir -p "$LOCALES_FOLDER/$name" &&
-    mv "$LOCALES_FOLDER/en/translation.$name.json" "$LOCALES_FOLDER/$name/translation.json" ||
-    touch $MISSING_FILE &&
-    grep -qxF $name $MISSING_FILE || echo $name >> $MISSING_FILE
+    node_modules/@parvineyvazov/json-translator/bin/jsontt "$LOCALES_FOLDER/en/translation.json" --module google --from en --to $name --name translation -cl 1 -fb yes
+
+    if [ $? -eq 0 ]; then    
+        mkdir -p "$LOCALES_FOLDER/$name"
+        mv "$LOCALES_FOLDER/en/translation.$name.json" "$LOCALES_FOLDER/$name/translation.json"
+    else
+        touch $MISSING_FILE &&
+        grep -qxF $name $MISSING_FILE || echo $name >> $MISSING_FILE
+    fi
 
 done <"$LOCALES_FOLDER/locales.txt"
