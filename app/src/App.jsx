6edb722b64837c43dcd0c65b81a79d1011f8465b
Oscar <oscarrc.web@gmail.com>
2024-01-08 17:19:26 +0100

    [WIP] Adblock detected page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,15 +1,17 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, redirect } from "react-router-dom";
 
 import { ChatProvider } from "./hooks/useChat";
 import Landing from "./routes/landing";
 import Layout from "./components/layout";
 import { lazy } from "react";
+import useDetectAdblock from "./hooks/useDetectAdblock";
 
 const App = () => {  
   const Chat = lazy(() => import('./routes/chat'));
   const Error = lazy(() => import('./routes/error'));
   const NotFound = lazy(() => import('./routes/error/404'));
   const Policies = lazy(() => import('./routes/policies'));
+  const adBlockDetected = useDetectAdblock();
   
   const router = createBrowserRouter([
     {
@@ -30,6 +32,7 @@ const App = () => {
           id: "unmoderated",
           path: "/unmoderated",
           element: <Chat />,
+          loader: () => adBlockDetected && redirect("/error")
         },
         {   
           id: "text",
