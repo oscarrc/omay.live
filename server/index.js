3df3ab90ecb6d22cc8e79ead3391ed0be1ab5264
Oscar <oscarrc.web@gmail.com>
2024-01-10 17:27:39 +0100

    [ADD] Cluster

--- server/index.js
+++ server/index.js
@@ -1,116 +1,55 @@
-import { BanService, ChatService } from "./services/index.js";
+import { Db, Server, Socket } from "./server.js";
 
-import bodyparser from "body-parser";
 import chalk from 'chalk';
-import cors from "cors";
+import cluster from "cluster"
 import dotenv from "dotenv";
-import express from "express";
-import mongoose from "mongoose";
+import http from "http";
+import os from "os";
 import router from "./router.js";
-import { Server as socket } from "socket.io";
-
-const PORT = process.env.PORT || 8080;
-const BASE_URL = process.env.BASE_URL || "localhost";
-const app = express(); 
+import { setupMaster } from "@socket.io/sticky";
+import { setupPrimary } from "@socket.io/cluster-adapter";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
-  console.log(`${chalk.green.bold("[DB]")} Connection ready`);
-  mongoose.connection.db.dropCollection("peers");
-})
+const PORT = process.env.PORT || 8080;
+const BASE_URL = process.env.BASE_URL || "localhost";
+const MONGO_URL = process.env.MONGO_URL || null;
+const WORKERS = process.env.WORKERS || os.cpus().length;
+const PRODUCTION = process.env.NODE_ENV || false;
 
-app.use(bodyparser.urlencoded({extended: true}))
-    .use(bodyparser.json())
-    .use(cors())
-    .use(router)
+if (cluster.isPrimary) {	
+	console.log(`${chalk.green.bold("[CLUSTER]")} Master running with PID ${process.pid}`);
 
-process.env.NODE_ENV === "production" && app.use(express.static("../app/dist"))
+	const server = http.createServer();
 
-const server = app.listen(PORT, () => {
-    console.log(`${chalk.green.bold("[HTTP]")} Ready on ${BASE_URL}:${PORT}`)
-})
+	setupMaster(server, { loadBalancingMethod: "least-connection" });
+	setupPrimary();
+	server.listen(PORT);
 
-const io = new socket(server, {
-    cors: {
-      origin: "*",
-      credentials: true
-    },
-});
+	for (let i = 0; i < WORKERS; i++) {
+		cluster.fork();
+	}
 
-if(io) console.log(`${chalk.green.bold("[SOCKET]")} Ready on ${BASE_URL}:${PORT}`)
+	cluster.on("online", (worker) => {		
+		console.log(`${chalk.green.bold("[CLUSTER]")} Worker ${worker.id} running with PID ${worker.process.pid}`);
+	})	
 
-io.on('connection', async (socket) => {
-  let ip =  socket.handshake.headers["true-client-ip"] || 
-            socket.handshake.headers["x-forwarded-for"]?.split(",")[0] || 
-            socket.client.conn.remoteAddress;
-
-  let banned = await BanService.isBanned(ip);
-     
-  if(banned) {      
-    console.log(`${chalk.red.bold("> [banned]")} ${chalk.bgRed.black.bold(' nsfw ')} ${chalk.red(`${socket.id} (${ip})`)}`)
-    socket.emit("banned");
-  }
-
-  await ChatService.peerConnected({
-    peer: socket.id,
-    ip: ip,
-    mode: socket.handshake.query.mode,
-    interests: socket.handshake.query.interests,
-    lang: socket.handshake.query.lang,
-    simulated: socket.handshake.query.simulated || false
-  })
-
-  console.log(`${chalk.green.bold("> [connected]")} ${chalk.bgGreen.black.bold(` ${socket.handshake.query.mode} `)} ${chalk.green(`${socket.id} (${ip})`)}`)
-
-  socket.on('report', async (data) => {
-    if(!data.id) return;
-
-    let peer = await ChatService.getPeer(data.id);
-    let banned = await BanService.warn(peer.ip);
-
-    console.log(`${chalk.yellow.bold("> [reported]")} ${chalk.bgYellow.black.bold(' nsfw ')} ${chalk.yellow(data.id)}`)
-    
-    if(!banned) return;
-
-    console.log(`${chalk.red.bold("> [banned]")} ${chalk.bgRed.black.bold(' nsfw ')} ${chalk.red(`${data.id} (${peer.ip})`)}`)
-    socket.emit("banned");
-
-    await ChatService.peerDisconnected(socket.id)
-  })
- 
-  socket.on('candidatesent', (data) => {
-    socket.to(data.remoteId).emit("receivecandidate", data)
-  })
-
-  socket.on('offercreated', async (data) => {
-    socket.to(data.remoteId).emit("receiveoffer", data);
-    await ChatService.peerAvailable(data.id);
-  })
-
-  socket.on('answercreated', async (data) => {    
-    await socket.to(data.remoteId).emit("receiveanswer", data)
-    ChatService.peerUnavailable(data.id);
-  })
-
-  socket.on('answerreceived', async (data) => {
-    await ChatService.peerUnavailable(data.id);
-  })
-
-  socket.on('connectionclosed', (data) => {
-    socket.to(data.remoteId).emit('peerdisconnected')
-  })
-
-  socket.on('disconnect', async () => {     
-    let ip =  socket.handshake.headers["true-client-ip"] || 
-              socket.handshake.headers["x-forwarded-for"]?.split(",")[0] || 
-              socket.client.conn.remoteAddress;
-              
-    await ChatService.peerDisconnected(socket.id)
-    console.log(`${chalk.blue.bold("> [disconnected]")} ${chalk.bgBlue.black.bold(" exit ")} ${chalk.blue(`${socket.id} (${ip})`)}`)
-  });
-
-  socket.on('peerupdated', async (data) => {
-    await ChatService.updatePeer(socket.id, data)
-  })
-});
+	cluster.on("exit", (worker) => {
+		console.log(`${chalk.red.bold("[CLUSTER]")} Worker died with PID ${worker.process.pid}`);
+		cluster.fork();
+	})	
+}else{
+	const purge = cluster.worker.id === 1;
+	
+	Db(MONGO_URL, purge).then(() => {
+		console.log(`${chalk.green.bold("[DB]")} Connection ready`);
+	
+		const server = Server(router, PRODUCTION).listen(PORT, () => {
+			console.log(`${chalk.green.bold("[HTTP]")} Ready on ${BASE_URL}:${PORT}`);
+			Socket(server);
+			console.log(`${chalk.green.bold("[SOCKET]")} Ready on ${BASE_URL}:${PORT}`);
+		})
+	}).catch((e) => {
+		console.log(`${chalk.red.bold("[ERROR]")} ${e}`);
+	})
+}
---
+++ server/server.js
@@ -0,0 +1,128 @@
+import { BanService, ChatService } from "./services/index.js";
+
+import { Server as SocketIO } from "socket.io";
+import bodyparser from "body-parser";
+import chalk from 'chalk';
+import cors from "cors";
+import { createAdapter } from "@socket.io/cluster-adapter";
+import express from "express";
+import mongoose from "mongoose";
+import { setupWorker } from "@socket.io/sticky";
+
+const app = express(); 
+
+const Db = (url, purge) => {
+    return new Promise( async (resolve, reject) => {
+        try{
+            await mongoose.connect(url);
+            if(purge){
+                let exists = await mongoose.connection.db.listCollections({name: "peers"}).toArray();
+                if(exists.length) mongoose.connection.db.dropCollection("peers");
+            }
+            resolve();
+        }catch(e){
+            reject(e)
+        }
+    })
+}
+
+const Server = (router, production) => {
+    app.use(bodyparser.urlencoded({extended: true}))
+        .use(bodyparser.json())
+        .use(cors())
+        .use(router)
+
+    production && app.use(express.static("../app/dist"))
+
+    return app
+}
+
+const Socket = (server) => {
+    const io = new SocketIO(server, {
+        cors: {
+          origin: "*",
+          credentials: true
+        },
+    });
+
+    io.adapter(createAdapter());
+
+    setupWorker(io);
+
+    io.on('connection', async (socket) => {
+        let ip =  socket.handshake.headers["true-client-ip"] || 
+                socket.handshake.headers["x-forwarded-for"]?.split(",")[0] || 
+                socket.client.conn.remoteAddress;
+    
+        let banned = await BanService.isBanned(ip);
+        
+        if(banned) {      
+            console.log(`${chalk.red.bold("> [banned]")} ${chalk.bgRed.black.bold(' nsfw ')} ${chalk.red(`${socket.id} (${ip})`)}`)
+            socket.emit("banned");
+        }
+    
+        await ChatService.peerConnected({
+            peer: socket.id,
+            ip: ip,
+            mode: socket.handshake.query.mode,
+            interests: socket.handshake.query.interests,
+            lang: socket.handshake.query.lang,
+            simulated: socket.handshake.query.simulated || false
+        })
+    
+        console.log(`${chalk.green.bold("> [connected]")} ${chalk.bgGreen.black.bold(` ${socket.handshake.query.mode} `)} ${chalk.green(`${socket.id} (${ip})`)}`)
+    
+        socket.on('report', async (data) => {
+            if(!data.id) return;
+        
+            let peer = await ChatService.getPeer(data.id);
+            let banned = await BanService.warn(peer.ip);
+        
+            console.log(`${chalk.yellow.bold("> [reported]")} ${chalk.bgYellow.black.bold(' nsfw ')} ${chalk.yellow(data.id)}`)
+            
+            if(!banned) return;
+        
+            console.log(`${chalk.red.bold("> [banned]")} ${chalk.bgRed.black.bold(' nsfw ')} ${chalk.red(`${data.id} (${peer.ip})`)}`)
+            socket.emit("banned");
+        
+            await ChatService.peerDisconnected(socket.id)
+        })
+    
+        socket.on('candidatesent', (data) => {
+         socket.to(data.remoteId).emit("receivecandidate", data)
+        })
+    
+        socket.on('offercreated', async (data) => {
+            socket.to(data.remoteId).emit("receiveoffer", data);
+            await ChatService.peerAvailable(data.id);
+        })
+    
+        socket.on('answercreated', async (data) => {    
+            await socket.to(data.remoteId).emit("receiveanswer", data)
+            ChatService.peerUnavailable(data.id);
+        })
+    
+        socket.on('answerreceived', async (data) => {
+            await ChatService.peerUnavailable(data.id);
+        })
+    
+        socket.on('connectionclosed', (data) => {
+            socket.to(data.remoteId).emit('peerdisconnected')
+        })
+    
+        socket.on('disconnect', async () => {     
+            let ip =  socket.handshake.headers["true-client-ip"] || 
+                        socket.handshake.headers["x-forwarded-for"]?.split(",")[0] || 
+                        socket.client.conn.remoteAddress;
+                        
+            await ChatService.peerDisconnected(socket.id)
+            console.log(`${chalk.blue.bold("> [disconnected]")} ${chalk.bgBlue.black.bold(" exit ")} ${chalk.blue(`${socket.id} (${ip})`)}`)
+        });
+    
+        socket.on('peerupdated', async (data) => {
+            await ChatService.updatePeer(socket.id, data)
+        })
+  });
+}
+
+export {Db, Server, Socket};
