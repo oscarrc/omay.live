91e87eaed12923b28a657c52c65e9e2a0a594e03
Oscar <oscarrc.web@gmail.com>
2024-01-11 12:07:43 +0100

    [FIX] __pathname is not defined

--- server/router.js
+++ server/router.js
@@ -1,9 +1,12 @@
 import { BanController, ChatController } from "./controllers/index.js";
 
 import express from "express";
+import { fileURLToPath } from 'url';
 import path from 'path'
 
 const router = express.Router();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 router.get("/", (req,res) => {
     process.env.NODE_ENV === "production" ? 
