5849715adfa640b4f695d2053a34e25f265fc474
Oscar <oscarrc.web@gmail.com>
2024-01-18 14:09:45 +0100

    [WIP] Video adblock alert

--- app/src/components/chat/VideoBox.jsx
+++ app/src/components/chat/VideoBox.jsx
@@ -1,16 +1,39 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { Loader } from "../partials";
+import useDetectAdblock from "../../hooks/useDetectAdblock";
+import { useTranslation } from "react-i18next";
 import useVast from "../../hooks/useVast";
 
 const VideoBox = ({ source, muted, className, loading, withAds, playAd, onAdStart, onAdEnd, onAdError }) => {
     const player = useRef(null);
     const container = useRef(null);
+    const adBlockDetected = useDetectAdblock();
+    const [ countdown, setCountDown ] = useState(10);
+    const { t } = useTranslation()
     const { adsManager, loadAd } = useVast(player, container, import.meta.env.VITE_VAST_TAG)
     
     useEffect(() => {
         player.current.srcObject = source;
     }, [source])
+
+    useEffect(() => {
+        if(!playAd || !adBlockDetected) return;
+        
+        let count = import.meta.env.VITE_ADBLOCK_TIMER * 100
+        let timer = setInterval(() => {
+            count -= 1
+            if (count > 0) setCountDown(count - 1);
+            else {
+                clearInterval(timer);
+                setCountDown(10);
+                onAdEnd();
+            }
+        }, 10);
+
+        onAdStart();
+        return () => { clearTimeout(timer) }
+    }, [playAd, adBlockDetected])
 
     useEffect(() => {
         if(!withAds || !adsManager) return;
@@ -36,7 +59,20 @@ const VideoBox = ({ source, muted, className, loading, withAds, playAd, onAdStar
         <div className={`flex items-center justify-center bg-neutral sm:rounded-lg shadow-inner overflow-hidden ${className}`}>            
             <video ref={player} autoPlay={true} playsInline={true} muted={muted} className="h-full w-auto" />
             { loading && <Loader className="absolute h-full top-0 left-0" /> }
-            { withAds && <div ref={container} className="absolute w-full h-full top-0 left-0"></div> }
+            { withAds && 
+                <div ref={container} className="absolute w-full h-full top-0 left-0">
+                    {
+                        playAd && adBlockDetected &&
+                            <div className="flex flex-col gap-4 justify-center items-center relative h-full w-full text-base-100">
+                                <h4 className="text-2xl">{t("common.alerts.adblockdetected")}</h4>
+                                <p>{t("common.alerts.disableadblock")}</p>
+                                <span className="absolute bottom-0 left-0 h-2 bg-primary" style={{
+                                    width: `${countdown / 10}%`
+                                }}> </span>
+                            </div>
+                    }
+                </div>
+            }
         </div>
     )
 }
