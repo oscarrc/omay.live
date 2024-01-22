f752ac2919ea2dc48aff487f201d9abc2aeb9aaf
Oscar <oscarrc.web@gmail.com>
2024-01-22 17:47:16 +0100

    [WIP] Fetch renderer script

--- server/controllers/Ad.controller.js
+++ server/controllers/Ad.controller.js
@@ -10,19 +10,6 @@ class AdController {
         const { zoneId } = req.query;
         const ip = req.ip || req.headers["true-client-ip"] || req.headers["x-forwarded-for"]?.split(",")[0];
         const ad = await this.service.getAds([zoneId], ip);
-
-        await Promise.all(
-            ad.zones.map( async (z) => {
-                if(!z.data?.image) return z;
-                
-                let res = await fetch(z.data.image, { mode : 'no-cors' });
-                let blob = await res.blob();
-                let arrayBuffer = await blob.arrayBuffer();
-                
-                z.data.image = `data:image/${z.data.image.split(".").pop()};base64,${Buffer.from(arrayBuffer).toString('base64')}`;
-                return z;
-            })
-        )
         
         return res.status(200).send(ad);
     }
--- server/services/Ad.service.js
+++ server/services/Ad.service.js
@@ -12,6 +12,25 @@ class AdService{
 
         const ads = await res.json();
 
+        await Promise.all(
+            ads.zones.map( async (z) => {
+                if(!z.data?.image) return z;
+                
+                let res = await fetch(z.data.image, { mode : 'no-cors' });
+                let blob = await res.blob();
+                let arrayBuffer = await blob.arrayBuffer();
+                
+                z.data.image = `data:image/${z.data.image.split(".").pop()};base64,${Buffer.from(arrayBuffer).toString('base64')}`;
+                return z;
+            })
+        )
+
+        // await Promise.all(
+        //     Object.keys(ads.renderers).forEach( async (k) => {
+        //         let res = await fetch(z.data.image, { mode : 'no-cors' });
+        //     })
+        // )
+
         return ads;
     }
 }
