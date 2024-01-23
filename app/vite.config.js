7f68fd31b0f7c8a98c24aa861258dcc67f7517d8
Oscar <oscarrc.web@gmail.com>
2024-01-23 09:39:50 +0100

    [MOD] RegExp URL

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,54 +1,59 @@
+import { defineConfig, loadEnv } from 'vite'
+
 import { VitePWA } from 'vite-plugin-pwa'
-import { defineConfig } from 'vite'
 import mdx from '@mdx-js/rollup'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-    base: '/',
-    plugins: [
-        {enforce: 'pre', ...mdx(/* jsxImportSource: …, otherOptions… */)},
-        react(),
-        VitePWA({ 
-            registerType: 'autoUpdate',
-            workbox: {
-                globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
-                maximumFileSizeToCacheInBytes: 3000000,
-                runtimeCaching: [
-                    {
-                        urlPattern: new RegExp(`^https:\/\/${import.meta.env.VITE_SERVER_URL}\/ad\/.*`, "i"),
-                        handler: 'NetworkFirst',
-                        options: {
-                            cacheName: 'ads-cache',
-                            expiration: {
-                                maxEntries: 10,
-                                maxAgeSeconds: 60 * 60 * 24 // <== 1 day
-                            },
-                            cacheableResponse: {
-                                statuses: [0, 200]
+export default ({ mode }) => {
+    let env = loadEnv(mode, process.cwd());
+
+    return defineConfig({
+        base: '/',
+        plugins: [
+            {enforce: 'pre', ...mdx(/* jsxImportSource: …, otherOptions… */)},
+            react(),
+            VitePWA({ 
+                registerType: 'autoUpdate',
+                workbox: {
+                    globPatterns: ['**/*.{js,css,html,ico,png,svg,json}'],
+                    maximumFileSizeToCacheInBytes: 3000000,
+                    runtimeCaching: [
+                        {
+                            urlPattern: new RegExp(`^${env.VITE_SERVER_URL}\/ad\/.*`, "i"),
+                            handler: 'NetworkFirst',
+                            options: {
+                                cacheName: 'ads-cache',
+                                expiration: {
+                                    maxEntries: 10,
+                                    maxAgeSeconds: 60 * 60 * 24 // <== 1 day
+                                },
+                                cacheableResponse: {
+                                    statuses: [0, 200]
+                                }
                             }
                         }
-                    }
-                ]
-            }
-        })
-    ],
-    server: {    
-        open: true, 
-        port: 3000,
-        host: true,
-        watch: {
-            usePolling: true
-        }
-    },
-    resolve: {
-        alias: [
-            {
-                find: 'nsfwjs',
-                replacement: 'nsfwjs/dist/nsfwjs.min.js',
-            },
+                    ]
+                }
+            })
         ],
-    },
-    build: {
-        chunkSizeWarningLimit: 600
-    }
-})
+        server: {    
+            open: true, 
+            port: 3000,
+            host: true,
+            watch: {
+                usePolling: true
+            }
+        },
+        resolve: {
+            alias: [
+                {
+                    find: 'nsfwjs',
+                    replacement: 'nsfwjs/dist/nsfwjs.min.js',
+                },
+            ],
+        },
+        build: {
+            chunkSizeWarningLimit: 600
+        }
+    })
+}
