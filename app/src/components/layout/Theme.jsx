43a67001ecd1ddd16ef6cf5eb5c617f378fb191e
Oscar <oscarrc.web@gmail.com>
2024-02-16 13:09:40 +0100

    [ADD] Aria--label to theme button

--- app/src/components/layout/Theme.jsx
+++ app/src/components/layout/Theme.jsx
@@ -2,7 +2,7 @@ import { IoMoonOutline, IoSunnyOutline } from "react-icons/io5";
 
 const Theme = ({ onClick, dark }) => {
     return (
-        <button onClick={onClick} className={`btn btn-sm btn-square swap swap-rotate btn-ghost border-base-content/25 ${dark && "swap-active"}`}>
+        <button onClick={onClick} aria-label="toggle light/dark theme" className={`btn btn-sm btn-square swap swap-rotate btn-ghost border-base-content/25 ${dark && "swap-active"}`}>
             <IoMoonOutline className="h-4 w-4 swap-on"/>
             <IoSunnyOutline className="h-4 w-4 swap-off"/>
         </button>
