685eb34c4f9a373f864fc534825529a691054db1
Oscar <oscarrc.web@gmail.com>
2024-02-17 12:12:56 +0100

    [ADD] resize arrows

--- app/src/components/partials/Resizable.jsx
+++ app/src/components/partials/Resizable.jsx
@@ -1,5 +1,7 @@
 import { forwardRef, useEffect } from "react";
 
+import { RiExpandLeftRightFill } from "react-icons/ri";
+
 const Resizable = forwardRef(({ children, className, resizeFunction }, ref) => {
     const handleResize = () => {
         resizeFunction && document.addEventListener('mousemove', resizeFunction);
@@ -16,7 +18,9 @@ const Resizable = forwardRef(({ children, className, resizeFunction }, ref) => {
     return (
         <div ref={ref} className={`resizable relative ${className}`}>
             { children }
-            <span onMouseDown={handleResize} className="handle hidden md:block" />
+            <span onMouseDown={handleResize} className="handle hidden relative md:block group">
+                <RiExpandLeftRightFill className="h-4 w-4 absolute top-1/2 text-base-content/20" />
+            </span>
         </div>
     )
 })
