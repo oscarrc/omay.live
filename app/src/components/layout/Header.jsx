fd5cd06c4dab83aa515d75800699772607f22465
Oscar <oscarrc.web@gmail.com>
2024-02-19 09:09:16 +0100

    [MOD] Logo font

--- app/src/components/layout/Header.jsx
+++ app/src/components/layout/Header.jsx
@@ -17,11 +17,11 @@ const Header = ({ children }) => {
         <header className="sticky top-0 bg-base-100 z-40">
             <nav className={`navbar transition-all duration-100	${scrolled ? "shadow-lg" : "shadow-none"}`}>
                 <div className="flex gap-8 min-w-fit flex-1">
-                    <Link to="/" className="btn btn-ghost text-warning text-xl md:text-2xl uppercase py-2">
+                    <Link to="/" className="logo btn btn-ghost text-warning text-2xl md:text-4xl py-2">
                         <img src="/logo.svg" alt={`${BRAND} logo`} className="h-full"/>
                         { BRAND }
                     </Link>
-                    <span className="hidden md:inline -rotate-6 text-primary text-sm lg:text-xl font-bold">Connect with strangers!</span>
+                    <span className="hidden md:inline -rotate-6 text-primary text-sm lg:text-xl font-bold">Live with strangers!</span>
                 </div>
                 <div className="flex gap-8 flex-shrink px-4">
                     { children }                      
