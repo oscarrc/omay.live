65ab4822cb4d6e40093575a0f0e0dfa208cb2613
Oscar <oscarrc.web@gmail.com>
2024-02-19 10:17:22 +0100

    [MOD] Animate user count

--- app/src/components/layout/Counter.jsx
+++ app/src/components/layout/Counter.jsx
@@ -1,7 +1,27 @@
+import{ useMemo } from "react";
+
 const Counter = ({ count }) => {
+    const counter = useMemo(() => {
+        let elements = [];
+        let counter = count.toString().match(/..?/g)
+
+        counter.forEach( (g,i) =>{
+            let c = counter.length % 2 !== 0 && i === counter.length - 1 ? "-mr-3" : "";
+
+            elements.push(           
+                <span className={`countdown text-xl md:text-2xl ${c}`} key={i}>
+                    <span style={{"--value":g}}></span>            
+                </span>
+            )
+        })
+        
+        return elements;
+    }, [count]);
+
     return (
         <div className="whitespace-nowrap text-primary text-lg font-bold hidden sm:inline">
-            <span className="text-2xl">{count}+</span> Online now
+            { counter }
+            <span className="text-2xl">+</span> Online now
         </div>
     )
 }
