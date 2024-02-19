379d29216bfb5a9562ed89b36a732be318ca0624
Oscar <oscarrc.web@gmail.com>
2024-02-19 10:21:41 +0100

    [MOD] countdown single digit

--- app/src/components/layout/Counter.jsx
+++ app/src/components/layout/Counter.jsx
@@ -3,13 +3,12 @@ import{ useMemo } from "react";
 const Counter = ({ count }) => {
     const counter = useMemo(() => {
         let elements = [];
-        let counter = count.toString().match(/..?/g)
+        let counter = count.toString().split("");
+        console.log(counter)
 
         counter.forEach( (g,i) =>{
-            let c = counter.length % 2 !== 0 && i === counter.length - 1 ? "-mr-3" : "";
-
             elements.push(           
-                <span className={`countdown text-xl md:text-2xl ${c}`} key={i}>
+                <span className="countdown countdown-single text-xl md:text-2xl" key={i}>
                     <span style={{"--value":g}}></span>            
                 </span>
             )
