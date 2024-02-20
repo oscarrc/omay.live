78c03f9511ff223c02c83005015bea7d147d5e8f
Oscar <oscarrc.web@gmail.com>
2024-02-20 10:48:30 +0100

    [MOD] Icon

--- app/src/components/layout/Header.jsx
+++ app/src/components/layout/Header.jsx
@@ -18,7 +18,7 @@ const Header = ({ children }) => {
             <nav className={`navbar transition-all duration-100	${scrolled ? "shadow-lg" : "shadow-none"}`}>
                 <div className="flex gap-8 min-w-fit flex-1">
                     <Link to="/" className="logo btn btn-ghost text-warning text-2xl md:text-4xl py-2">
-                        <img src="/logo.svg" alt={`${BRAND} logo`} className="h-full"/>
+                        <img src="/logo.svg" alt={`${BRAND} logo`} className="h-full rounded-md"/>
                         { BRAND }
                     </Link>
                     <span className="hidden md:inline -rotate-6 text-primary text-sm lg:text-xl font-bold">Live with strangers!</span>
