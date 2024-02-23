817ce62ac05964ee02550339e9519bcb4d362624
Oscar <oscarrc.web@gmail.com>
2024-02-23 10:59:23 +0100

    [MOD] Light / dark theme icons

--- app/src/components/layout/Theme.jsx
+++ app/src/components/layout/Theme.jsx
@@ -1,10 +1,10 @@
-import { IoMoonOutline, IoSunnyOutline } from "react-icons/io5";
+import { LuLightbulb, LuLightbulbOff } from "react-icons/lu";
 
 const Theme = ({ onClick, dark }) => {
     return (
-        <button onClick={onClick} aria-label="toggle light/dark theme" className={`btn btn-sm btn-square swap swap-rotate btn-ghost border-base-content/25 ${dark && "swap-active"}`}>
-            <IoMoonOutline className="h-4 w-4 swap-on"/>
-            <IoSunnyOutline className="h-4 w-4 swap-off"/>
+        <button onClick={onClick} aria-label="toggle light/dark theme" className={`btn btn-sm btn-square swap btn-ghost border-base-content/25 ${dark && "swap-active"}`}>
+            <LuLightbulb className="h-4 w-4 swap-on"/>
+            <LuLightbulbOff className="h-4 w-4 swap-off"/>
         </button>
     )
 }
