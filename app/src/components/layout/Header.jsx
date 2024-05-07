94ba4d40e3fd85e5371733eba28e14d1b7c427c3
Oscar <oscarrc.web@gmail.com>
2024-05-07 10:19:44 +0200

    [ADD] Beta badge

--- app/src/components/layout/Header.jsx
+++ app/src/components/layout/Header.jsx
@@ -17,9 +17,10 @@ const Header = ({ children }) => {
         <header className="sticky top-0 bg-base-100 z-40">
             <nav className={`navbar transition-all duration-100	${scrolled ? "shadow-lg" : "shadow-none"}`}>
                 <div className="flex gap-8 min-w-fit flex-1">
-                    <Link to="/" className="logo btn btn-ghost text-warning text-2xl md:text-4xl py-2">
+                    <Link to="/" className="indicator logo btn btn-ghost text-warning text-2xl md:text-4xl py-2 relative">
                         <img src="/logo.svg" alt={`${BRAND} logo`} className="h-full rounded-md"/>
                         { BRAND }
+                        { import.meta.env.VITE_IS_BETA ? <span class="indicator-item badge badge-primary badge-xs text-white">beta</span>  : "" }
                     </Link>
                     <span className="hidden md:inline -rotate-6 text-primary text-sm lg:text-xl font-bold">Live with strangers!</span>
                 </div>
